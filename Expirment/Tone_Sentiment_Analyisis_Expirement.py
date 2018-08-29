6fd4055ccc8278856e9895846325249d51511659
Rafael Pilot Linux Subsystem <rpilotojr@icloud.com>
2018-08-29 19:32:02 -0400

    Commented Code

--- Expirment/Tone_Sentiment_Analyisis_Expirement.py
+++ Expirment/Tone_Sentiment_Analyisis_Expirement.py
@@ -1,15 +1,29 @@
-import textblob
-from textblob.sentiments import NaiveBayesAnalyzer
+"""
+	Created by: Rafael Piloto 8.29.2018
+	
+	Sentiment Analyisis Experiement
+		- I wanted to create a program that will try to predict the answer to Tone / Attitude English Questions
+		  using sentiment Analysis
+	Uses TextBlob
 
-passage1 = open("passage01.txt", "r").read()
+"""
+
+
+import textblob
+from textblob.sentiments import NaiveBayesAnalyzer # Optional - UNUSED
+
+# Open passages
+
+passage1 = open("passage01.txt", "r").read() 
 passage2 = open("passage02.txt", "r").read()
 
+# Answers to the first passage from College Board AP English Exam
 passage1_answers = ["Evenhanded and logical",
            "Whimsical and poetic",
            "Imprecise and accusative",
            "Sarcastic and biting",
            "Paranoid and skeptical"]
-
+# Answers to the second passage from College Board AP English Exam
 passage2_answers = ["He was sardonic",
                     "He was apathetic",
                     "He was offended",
@@ -17,10 +31,11 @@ passage2_answers = ["He was sardonic",
                     "He was earnest"]
 
 def evaluatePassage(passage, answers):
-
-	blob = textblob.TextBlob(passage)
-	blob_answers = [textblob.TextBlob(x) for x in answers]
-	sent = blob.sentiment
+	# Function that will preform sentiment analysis on a passage & its answers
+	# Calculates 2 possible answers whose sentiment polarity closely relates to that of the passage
+	blob = textblob.TextBlob(passage) # Passage
+	blob_answers = [textblob.TextBlob(x) for x in answers] # Answers
+	sent = blob.sentiment # Passage Sentiment
 
 	print("Passage possible Tone answers:")
 
@@ -30,16 +45,18 @@ def evaluatePassage(passage, answers):
 	for answer in blob_answers:
 		print("{} - {}".format(answer, answer.sentiment))
 		if greatest and sent.polarity < answer.sentiment.polarity < greatest.sentiment.polarity:
+			# If the polarity is greater than the passage but less than all the other polarities
 			greatest = answer
 		elif answer.sentiment.polarity > sent.polarity:
-			greatest = answer
+			greatest = answer # No other answer previously recorded. Set the first greatest one
 		if lowest and sent.polarity > answer.sentiment.polarity > lowest.sentiment.polarity:
+			# If the polarity is lower than the passage but less than all the other polarities
 			lowest = answer
 		elif answer.sentiment.polarity < sent.polarity:
-			lowest = answer
+			lowest = answer # No other answer previously recorded. Set the first lowest one
 		
 	print("Passage has a sentiment polarity of: {} ".format(sent), end="\n\n")
 	print("The Analysis best calculates '{}' and '{}' to be the best possible answers".format(lowest, greatest), end = "\n\n")
 
-evaluatePassage(passage1, passage1_answers)
+evaluatePassage(passage1, passage1_answers) # Adjust based on passage and answers
 
