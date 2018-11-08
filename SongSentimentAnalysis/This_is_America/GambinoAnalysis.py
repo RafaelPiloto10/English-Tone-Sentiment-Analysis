55fd5b8d56d106f91a45c825aa81e02869e76b58
Rafael Piloto <piloto.1@new-albany.k12.oh.us>
2018-11-08 12:17:24 -0500

    Updated README. Fixed Gambino Y axis title out of view

--- SongSentimentAnalysis/This_is_America/GambinoAnalysis.py
+++ SongSentimentAnalysis/This_is_America/GambinoAnalysis.py
@@ -8,17 +8,17 @@ subjectivity = []
 with open("./GambinoSong.txt") as f:
   [(polarity.append(TextBlob(line).sentiment.polarity), subjectivity.append(TextBlob(line).subjectivity)) for line in f.read().split("\n") if line != "" and TextBlob(line).polarity != 0]
 
-def plot(ax, data, label, fontsize=12, axis=None):
-     ax.plot(data)
-     ax.locator_params(nbins=3)
+def plot(p, data, label, fontsize=12, axis=None):
+     p.plot(data)
+     p.locator_params(nbins=3)
      if axis is not None:
-       ax.axis(axis)
-     ax.set_xlabel("LINES", fontsize=fontsize)
-     ax.set_ylabel(label, fontsize=fontsize)
+       p.axis(axis)
+     p.set_xlabel("LINES", fontsize=fontsize)
+     p.set_ylabel(label, fontsize=fontsize)
 
 fig, sub = plt.subplots(2, 1, constrained_layout=True)
 fig.suptitle("Sentiment Analyisis of 'This is America'")
-plot(sub[0], polarity, "POLARITY (-1, 1)", .1)
+plot(sub[0], polarity, "POLARITY (-1, 1)")
 plot(sub[1], subjectivity, "SUBJECTIVITY (0 -1)")
 
 
