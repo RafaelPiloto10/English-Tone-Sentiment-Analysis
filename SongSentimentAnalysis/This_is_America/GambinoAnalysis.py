3ad44335edc9e7c96c10c1ceb1d27045c2bf82fd
Rafael Piloto <piloto.1@new-albany.k12.oh.us>
2018-11-09 10:16:21 -0500

    Changed styles, added a save image option. Data info output

--- SongSentimentAnalysis/This_is_America/GambinoAnalysis.py
+++ SongSentimentAnalysis/This_is_America/GambinoAnalysis.py
@@ -1,25 +1,54 @@
 from textblob import TextBlob
 import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
+import time
 
-plt.style.use("fivethirtyeight")
+start = time.time()
+print(plt.style.available)
+plt.style.use("seaborn-talk") # _classic_test, fivethirtyeight, classic, bmh, seaborn-talk
+
+loc = plticker.MultipleLocator(base=.3)
 
 polarity = []
 subjectivity = []
-with open("./GambinoSong.txt") as f:
-  [(polarity.append(TextBlob(line).sentiment.polarity), subjectivity.append(TextBlob(line).subjectivity)) for line in f.read().split("\n") if line != "" and TextBlob(line).polarity != 0]
 
-def plot(ax, data, label, fontsize=12, axis=None):
-     ax.plot(data)
-     ax.locator_params(nbins=3)
-     if axis is not None:
-       ax.axis(axis)
-     ax.set_xlabel("LINES", fontsize=fontsize)
-     ax.set_ylabel(label, fontsize=fontsize)
+lines = []
+polarityEqualsZero = 0
+
+with open("./GambinoSong.txt") as f:
+  for line in f.read().split("\n"):
+    if line != "" and line not in lines:
+      sentiment = TextBlob(line)
+      if sentiment.sentiment.polarity != 0:
+        polarity.append(sentiment.sentiment.polarity)
+      else:
+        polarityEqualsZero += 1
+        polarity.append(sentiment.sentiment.polarity)
+      subjectivity.append(sentiment.subjectivity)
+      lines.append(line)
+      
+def plot(p, data, label, fontsize=12):
+     p.plot(data)
+     p.locator_params(nbins=3)
+     p.set_xlabel("LINES", fontsize=fontsize)
+     p.set_ylabel(label, fontsize=fontsize)
 
 fig, sub = plt.subplots(2, 1, constrained_layout=True)
 fig.suptitle("Sentiment Analyisis of 'This is America'")
-plot(sub[0], polarity, "POLARITY (-1, 1)", .1)
-plot(sub[1], subjectivity, "SUBJECTIVITY (0 -1)")
 
+sub[0].axis([0, len(polarity), -1.0, 1.0])
+plot(sub[0], polarity, "POLARITY (-1, 1)")
+sub[0].yaxis.set_major_locator(loc)
+sub[0].xaxis.set_major_locator(plticker.MultipleLocator(base=5))
+
+
+plot(sub[1], subjectivity, "SUBJECTIVITY (0 -1)")
+sub[1].xaxis.set_major_locator(plticker.MultipleLocator(base=5))
+
+# plt.savefig("analysis.png")
+print("""
+Completed in {:.02f}s. Total lines analyzed: {}.\n
+Lines with canceling polarity: {}.
+\nTotal lines with polarity: {}.""".format(time.time() - start, len(set(lines)), polarityEqualsZero, len(set(lines)) - polarityEqualsZero))
 
 plt.show()
